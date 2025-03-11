f3f208ef6891432cbbbce3b6dcff88faf9fce719
George-01 <george.coker@hotmail.com>
2025-03-11 11:56:02 +0000

    implement repository pattern  with marten for data management add exception handling

--- src/Services/Basket/BAsket.API/Program.cs
+++ src/Services/Basket/BAsket.API/Program.cs
@@ -1,10 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
+var assembly = typeof(Program).Assembly;
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
+builder.Services.AddMarten(opt =>
+{
+    opt.Connection(builder.Configuration.GetConnectionString("Database")!);
+    opt.Schema.For<ShoppingCart>().Identity(x => x.UserName);
+}).UseLightweightSessions();
 
 // Add services to the container.
+builder.Services.AddCarter();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+app.MapCarter();
 
 app.Run();
