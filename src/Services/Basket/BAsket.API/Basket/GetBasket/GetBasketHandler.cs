63e87f92ba20a690fa33c75d82d04e7362334ac1
George-01 <george.coker@hotmail.com>
2025-03-11 09:58:44 +0000

    implemented getBasket handler and endpoints

--- src/Services/Basket/BAsket.API/Basket/GetBasket/GetBasketHandler.cs
+++ src/Services/Basket/BAsket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -1,6 +1,17 @@
 ﻿namespace Basket.API.Basket.GetBasket
 {
-    public class GetBasketHandler
+    public record GetBasketQuery(string Username) : IQuery<GetBasketResult>;
+
+    public record GetBasketResult(ShoppingCart Cart);
+
+    public class GetBasketQueryHandler : IQueryHandler<GetBasketQuery, GetBasketResult>
     {
+        public async Task<GetBasketResult> Handle(GetBasketQuery request, CancellationToken cancellationToken)
+        {
+            //ToDo: get basket from db
+            //var basket = await _repository.GetBasket(request.Username);
+
+            return new GetBasketResult(new ShoppingCart("test_svn"));
+        }
     }
 }
