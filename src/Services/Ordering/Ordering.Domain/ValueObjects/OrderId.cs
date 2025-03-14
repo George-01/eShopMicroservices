e712f32676b5a4d5cad814c72b651454b9b4cc0a
George-01 <george.coker@hotmail.com>
2025-03-14 17:10:23 +0000

    implemented all ordering domain layer with model entities and value objects using Tactical DDD and Events

---
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/OrderId.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.NetworkInformation;
+
+namespace Ordering.Domain.ValueObjects
+{
+    public record OrderId
+    {
+        public Guid Value { get; }
+
+        private OrderId(Guid value) => Value = value;
+
+        public static OrderId Of(Guid value)
+        {
+            ArgumentNullException.ThrowIfNull(value);
+            if (value == Guid.Empty)
+            {
+                throw new DomainException("Order id cannot be empty.");
+            }
+            return new OrderId(value);
+        }
+    }
+}
