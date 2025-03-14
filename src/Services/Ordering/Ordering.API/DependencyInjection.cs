29e8bc2671499211db2c6363f76f7f9ad81dbdee
George-01 <george.coker@hotmail.com>
2025-03-14 11:02:40 +0000

    configured and setup the ordering module for clean architecture and DDD pattern

---
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Marten.Events;
+
+namespace Ordering.API
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApiServices(this IServiceCollection services)
+        {
+            //services.Carter();
+
+            return services;
+        }
+
+        public static WebApplication UseApiServices(this WebApplication app)
+        {
+            //app.UseCarter();
+
+            return app;
+        }
+    }
+}
