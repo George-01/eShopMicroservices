42abfd5f357fb60f31eab9220a5eb3d15d2c2935
George-01 <george.coker@hotmail.com>
2025-03-17 14:58:53 +0000

    implemented savechangesInterceptor auditing for Order entities

--- src/Services/Ordering/Ordering.Infrastructure/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Infrastructure/GlobalUsing.cs
@@ -1,4 +1,7 @@
-﻿global using Ordering.Domain.Models;
+﻿global using Ordering.Infrastructure.Data;
+global using Ordering.Domain.Models;
+global using Ordering.Infrastructure.Data.Interceptors;
 global using Ordering.Domain.ValueObjects;
 global using Microsoft.EntityFrameworkCore;
-global using Ordering.Infrastructure.Data;
+global using Ordering.Domain.Abstractions;
+
