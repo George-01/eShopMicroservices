d10917312a4f406f0c497f121b7b83479ab19f46
George-01 <george.coker@hotmail.com>
2025-03-18 04:52:06 +0000

    created IApplicationDbContext to inject efcore db context objects in ordering app layer

--- src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderHandler.cs
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderHandler.cs
@@ -2,7 +2,7 @@
 
 namespace Ordering.Application.Orders.Commands.CreateOrder
 {
-    public class CreateOrderHandler : ICommandHandler<CreateOrderCommand, CreateOrderResult>
+    public class CreateOrderHandler() : ICommandHandler<CreateOrderCommand, CreateOrderResult>
     {
         public Task<CreateOrderResult> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
         {
