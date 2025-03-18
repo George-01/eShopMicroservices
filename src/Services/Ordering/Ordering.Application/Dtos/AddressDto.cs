ee3a5b8e491730547c707143b86259c5c57d74be
George-01 <george.coker@hotmail.com>
2025-03-18 04:16:24 +0000

    setup ordering app layer structure and required dtos

---
+++ src/Services/Ordering/Ordering.Application/Dtos/AddressDto.cs
@@ -0,0 +1,5 @@
+﻿namespace Ordering.Application.Dtos
+{
+    public record AddressDto(string FirstName, string LstName, 
+        string EmailAddress, string Addressline, string Country, string State, string ZipCode);
+}
