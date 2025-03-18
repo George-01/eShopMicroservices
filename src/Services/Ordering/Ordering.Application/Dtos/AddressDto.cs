4a93127fcaa4c43e422431211835ab06eac53d9c
George-01 <george.coker@hotmail.com>
2025-03-18 05:24:06 +0000

    implemented createOrderCommandHandler using IApplicationDbContext

--- src/Services/Ordering/Ordering.Application/Dtos/AddressDto.cs
+++ src/Services/Ordering/Ordering.Application/Dtos/AddressDto.cs
@@ -1,5 +1,5 @@
 ﻿namespace Ordering.Application.Dtos
 {
-    public record AddressDto(string FirstName, string LstName, 
+    public record AddressDto(string FirstName, string LastName, 
         string EmailAddress, string Addressline, string Country, string State, string ZipCode);
 }
