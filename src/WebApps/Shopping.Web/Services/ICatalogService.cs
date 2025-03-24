f41a3482516e82400fefcbca0f043e35da9a3241
George-01 <george.coker@hotmail.com>
2025-03-24 11:01:31 +0000

    created service classes to consume yarp api gateway endpoints using ReFit

---
+++ src/WebApps/Shopping.Web/Services/ICatalogService.cs
@@ -0,0 +1,9 @@
+﻿namespace Shopping.Web.Services
+{
+    public interface ICatalogService
+    {
+        Task<GetProductsResponse> GetProducts(int? PageNumber = 1, int? PageSize = 10);
+        Task<GetProductByIdResponse> GetProductById(Guid id);
+        Task<GetProductByCategoryResponse> GetProductByCategory(string category);
+    }
+}
