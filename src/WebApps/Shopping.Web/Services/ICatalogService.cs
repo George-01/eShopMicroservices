c3fb21c2945e767478277aedb316207ecc5665b8
George-01 <george.coker@hotmail.com>
2025-03-24 11:40:11 +0000

    added annotations to ICatalogService methods of the web app

--- src/WebApps/Shopping.Web/Services/ICatalogService.cs
+++ src/WebApps/Shopping.Web/Services/ICatalogService.cs
@@ -2,8 +2,13 @@
 {
     public interface ICatalogService
     {
+        [Get("/catalog-service/products?pageNumber={pageNumber}&pageSize={pageSize}")]
         Task<GetProductsResponse> GetProducts(int? PageNumber = 1, int? PageSize = 10);
+        
+        [Get("/catalog-service/products/{id}")]
         Task<GetProductByIdResponse> GetProductById(Guid id);
+
+        [Get("/catalog-service/products/category/{category}")]
         Task<GetProductByCategoryResponse> GetProductByCategory(string category);
     }
 }
